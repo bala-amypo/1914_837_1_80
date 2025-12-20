a687b1de6c8892c54a8725fccf9d65da84c8e02b
bala-amypo <bala@amypo.in>
2025-12-20 09:44:47 +0000

    User commit

--- src/main/java/com/example/demo/service/ClashDetectionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.entity.AcademicEvent;
-
-public interface ClashDetectionService {
-
-    boolean hasClash(List<AcademicEvent> events);
-}
--- src/main/java/com/example/demo/service/EventMergeService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.entity.AcademicEvent;
-
-public interface EventMergeService {
-
-    List<AcademicEvent> mergeEvents(List<AcademicEvent> events);
-}
