bdef3cd6de3b7d499a849e6c6eae7cc1d01b45d0
bala-amypo <bala@amypo.in>
2025-12-20 04:32:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEvent.java
+++ src/main/java/com/example/demo/entity/AcademicEvent.java
@@ -1,14 +1,14 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
-import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
-@Entity
-@Table(name = "academic_events")
-public class AcademicEvent {
+import java.time.LocalDate;
 
+
+@Entity
+public class AcademicEvent {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long branchId;
     private String title;
@@ -18,10 +18,55 @@ public class AcademicEvent {
     private String location;
     private String description;
     private LocalDateTime submittedAt;
-    public AcademicEvent() {}
+
+    public void ValidateAndInitializer() {
+        this.submittedAt = LocalDateTime.now();
+        this.startDate = LocalDate.now();
+        this.endDate = LocalDate.now();
+        if (startDate != null && endDate != null && startDate.isAfter(endDate)) {
+            throw new RuntimeException("ValidationException: startDate must be before or equal to endDate.");
+    }
+    }
     
-    public AcademicEvent(Long id, Long branchId, String title, String eventType,LocalDate startDate, LocalDate endDate,
-    String location, String description, LocalDateTime submittedAt) {
+    public Long getId() { 
+        return id; 
+    }
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+
+    public Long getBranchId() { 
+        return branchId;
+    }
+    public void setBranchId(Long branchId) 
+    { 
+        this.branchId = branchId;
+    }
+
+    public String getTitle() 
+    { 
+        return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getEventType() { return eventType; }
+    public void setEventType(String eventType) { this.eventType = eventType; }
+
+    public LocalDate getStartDate() { return startDate; }
+    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
+
+    public LocalDate getEndDate() { return endDate; }
+    public void setEndDate(LocalDate endDate) { this.endDate = endDate; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDateTime getSubmittedAt() { return submittedAt; }
+    public void setSubmittedAt(LocalDateTime submittedAt) { this.submittedAt = submittedAt; }
+    
+    public AcademicEvent(Long id, Long branchId, String title, String eventType, LocalDate startDate, LocalDate endDate, String location, String description, LocalDateTime submittedAt) {
         this.id = id;
         this.branchId = branchId;
         this.title = title;
@@ -32,9 +77,7 @@ public class AcademicEvent {
         this.description = description;
         this.submittedAt = submittedAt;
     }
-    @PrePersist
-    public void onCreate() {
-        this.submittedAt = LocalDateTime.now();
+    public AcademicEvent() {
+
     }
-    // Getters and Setters
 }
