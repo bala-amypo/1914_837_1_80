55598cd5d6ee29328e420a9db112212b49aa25c5
bala-amypo <bala@amypo.in>
2025-12-20 05:08:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/EventMergeRecord.java
+++ src/main/java/com/example/demo/entity/EventMergeRecord.java
@@ -7,31 +7,28 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "event_merge_records")
 public class EventMergeRecord {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String sourceEventIds;
-
     private String mergedTitle;
-
     private LocalDate mergedStartDate;
-
     private LocalDate mergedEndDate;
-
     private String mergeReason;
-
     private LocalDateTime createdAt;
-
     public EventMergeRecord() {}
-
+    public EventMergeRecord(Long id, String sourceEventIds, String mergedTitle,  LocalDate mergedStartDate, LocalDate mergedEndDate,
+String mergeReason, LocalDateTime createdAt) {
+        this.id = id;
+        this.sourceEventIds = sourceEventIds;
+        this.mergedTitle = mergedTitle;
+        this.mergedStartDate = mergedStartDate;
+        this.mergedEndDate = mergedEndDate;
+        this.mergeReason = mergeReason;
+        this.createdAt = createdAt;
+    }
     @PrePersist
     public void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
-
-    public Long getId() { return id; }
-    public String getSourceEventIds() { return sourceEventIds; }
-    public void setSourceEventIds(String sourceEventIds) { this.sourceEventIds = sourceEventIds; }
 }
