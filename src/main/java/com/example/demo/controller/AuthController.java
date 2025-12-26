5bdf3448bcd644deb8f5183a70b24cc1d00d32d4
bala-amypo <bala@amypo.in>
2025-12-26 13:00:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,25 +1,25 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.security.JwtUtil;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.security.JwtUtil;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+// import java.util.Map;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
 
-    private final JwtUtil jwtUtil;
+//     private final JwtUtil jwtUtil;
 
-    public AuthController(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+//     public AuthController(JwtUtil jwtUtil) {
+//         this.jwtUtil = jwtUtil;
+//     }
 
-    @GetMapping("/token")
-    public String getToken() {
-        return jwtUtil.generateToken(
-                Map.of("role", "ADMIN", "userId", 1L),
-                "test@gmail.com"
-        );
-    }
-}
+//     @GetMapping("/token")
+//     public String getToken() {
+//         return jwtUtil.generateToken(
+//                 Map.of("role", "ADMIN", "userId", 1L),
+//                 "test@gmail.com"
+//         );
+//     }
+// }
